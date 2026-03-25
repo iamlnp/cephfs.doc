be8bfb2d5d2bb32bb448dc46e09f18a6b10ac877
liunp <iamlnp@163.com>
2026-03-26 00:04:12 +0800

    调整导航方式

--- sync_fils.sh
+++ sync_fils.sh
@@ -1,1 +1,2 @@
 rsync -av --delete --exclude=".DS_Store" --exclude="*.tmp" --exclude="*.cache" --iconv=utf-8-mac,utf-8 --modify-window=1 /Users/liunaipeng/Documents/SynologyDrive/local-notes/【01】技术系统/【01】通用技术/存储/ceph/文件系统/cephfs/* /Users/liunaipeng/Documents/code/cephfs.doc/docs/
+mv cephfs总览.md index.md
